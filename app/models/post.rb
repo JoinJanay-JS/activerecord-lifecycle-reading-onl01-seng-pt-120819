563334b1153a75197cc4f66e3dc3c64122843b51
Janay Bailey <tjanay87@gmail.com>
2020-06-01 11:55:38 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ActiveRecord::Base
   private
   
   before_validation :make_title_case
+  before_save :email_author_about_post
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
